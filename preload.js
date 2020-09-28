8dc5dd3618bcadcb6593ffecd76a95eaea4d9183
me10zyl@qq.com <12345678>
2020-09-28 19:37:44 +0800

    add vue

--- preload.js
+++ preload.js
@@ -1,9 +1,3 @@
 window.loadConfigPage = function () {
     utools.redirect('jenkins-set', '')
 }
-utools.onPluginEnter(({code, type, payload, optional}) => {
-    console.log('用户进入插件', code, type, payload)
-    if(code === 'jenkins-set'){
-        document.write(require('fs').readFileSync(location.href.replace("file:///", "").replace('index.html', 'jenkins-config.html')))
-    }
-})
